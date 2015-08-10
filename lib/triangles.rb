9a4906bd7bb52f0fac2ed91992c484d180e9fe66
Andrew Dang & Kyle Mellander <student@epicodus.com>
2015-08-10 09:51:34 -0700

    create methods for area, height, width_1, and width_2

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -21,4 +21,21 @@ class Triangle
   def scalene?
     isosceles? == false
   end
+
+  def area
+    s = (@side_a + @side_b + @side_c) / 2
+    sqrt(s * (s - @side_a) * (s - @side_b) * (s - @side c)
+  end
+
+  def height
+    2 * area() / @side_c
+  end
+
+  def width_1
+    @side_c - (sqrt((@side_a ^ 2) - (height() ^ 2))
+  end
+
+  def width_2
+    @side_c - width_1
+  end
 end
